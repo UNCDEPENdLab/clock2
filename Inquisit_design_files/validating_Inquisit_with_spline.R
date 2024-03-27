8c408a54cb0eb77e38d0c349d1d5b6688fc8e248
andrewpapale <papalae04@gmail.com>
2024-03-27 17:52:40 -0400

    working on validating inquisit with spline

---
+++ Inquisit_design_files/validating_Inquisit_with_spline.R
@@ -0,0 +1,4 @@
+# 2024-03-27 AndyP
+# Spline in Troll World using extracted value vector
+# without erasures, and pulling out points from erasures 
+# to then reimplement spline one erasure at a time the way it is done in Inquisit
