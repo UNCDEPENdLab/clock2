248e60c41d0b433b405a8772d5a3ce6458dc78c0
andrewpapale <papalae04@gmail.com>
2025-02-04 09:56:17 -0500

    Update check_90_degree_CCW_shift.R

--- code/Code_related_to_Inquisit_and_Validation/check_90_degree_CCW_shift.R
+++ code/Code_related_to_Inquisit_and_Validation/check_90_degree_CCW_shift.R
@@ -9,21 +9,25 @@ library(pracma)
 
 design_file <- '/Users/andypapale/clock2/Inquisit_design_files/Design-Matrix-152.csv'
 d152 <- read_csv(design_file)
-df1 <- df0 %>% filter(seed=='152')
-df1 <- df1 %>% mutate(resp_theta_deg = case_when(pos_shifted < 90 ~ (pos_shifted + 270),
+df_152 <- df0 %>% filter(seed=='152')
+df_152 <- df_152 %>% mutate(resp_theta_deg = case_when(pos_shifted < 90 ~ (pos_shifted + 270),
                                                  pos_shifted >= 90 ~ (pos_shifted - 90)))
-df2 <- df1 %>% mutate(RT = round(resp_theta_deg))
-df_correct <- inner_join(d152,df2,by=c('trial','RT'))
+df_152_correct <- df_152 %>% mutate(RT = round(resp_theta_deg))
+df_152_correct <- inner_join(d152,df_152_correct,by=c('trial','RT'))
 
-hist(df_correct$value-df_correct$mag,breaks=100)
+hist(df_152_correct$value-df_152_correct$mag,breaks=100)
 
-df_mismatch_152_correct = df_correct %>% filter(df_correct$value - df_correct$mag != 0)
+df_mismatch_152_correct = df_152_correct %>% filter(df_152_correct$value - df_152_correct$mag != 0)
 
-df3 <- df1 %>% mutate(RT = round(pos_shifted))
-df_incorrect <- inner_join(d152,df3,by=c('trial','RT'))
+hist(df_mismatch_152_correct$value - df_mismatch_152_correct$mag, breaks=100)
 
-hist(df_incorrect$value-df_incorrect$mag,breaks=100)
+df_152_incorrect <- df_152 %>% mutate(RT = round(pos_shifted))
+df_152_incorrect <- inner_join(d152,df_152_incorrect,by=c('trial','RT'))
 
-df_mismatch_152_incorrect <- df_incorrect %>% filter(df_incorrect$value - df_incorrect$mag != 0)
+hist(df_152_incorrect$value-df_152_incorrect$mag,breaks=100)
+
+df_mismatch_152_incorrect <- df_152_incorrect %>% filter(df_152_incorrect$value - df_152_incorrect$mag != 0)
+
+hist(df_mismatch_152_incorrect$value - df_mismatch_152_incorrect$mag, breaks=100)
 
                   
